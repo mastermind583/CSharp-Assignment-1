84398303d6f84c47280b1dc00a2f18ad32b1d36c
Jacob <mastermind583@gmail.com>
2021-09-08 19:06:24 -0400

    finished assignment

---
+++ Library.TaskManager/Task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Library.TaskManager
+{
+    public class Task
+    {
+        private static int currentId;
+
+        public Task()
+        {
+            Id = currentId++;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {Id} - DEADLINE: {Deadline} - NAME: {Name} - DESCRIPTION: {Description} - COMPLETED: {IsCompleted}";
+        }
+    }
+}
